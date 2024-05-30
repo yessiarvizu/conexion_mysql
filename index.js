94a86b5a508e08a1de55c4f34b3c177128716bd5
yessiarvizu <146871156+yessiarvizu@users.noreply.github.com>
2024-05-30 09:46:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 4000;;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en http://localhost:" + PORT);
